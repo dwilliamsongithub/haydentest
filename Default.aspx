5a9f2fa17b18ca582e6db07e8b10b7e44026aeac
Dave Williamson <dwilliamson@innovateteam.com>
2016-06-02 16:10:00 -0700

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Title="Business Licensing Homepage" Language="C#" MasterPageFile="~/Master1.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        .style1 {
+            font-size: medium;
+        }
+        .style2
+        {
+            font-size: 9pt;
+        }
+        .style3
+        {
+            text-decoration: underline;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <p class="style1">
+        &nbsp;<strong>Welcome</strong>
+    </p>
+    <p>
+        &nbsp;If you have recieved a letter from the City of Hayden with your Business 
+        License ID and PIN numbers, please provide those numbers in the 
+        boxes below to begin the licensing process.
+    </p>
+
+    <table>
+        <tr>
+            <td class="style2" align="center"><strong>Business ID</strong></td>
+            <td class="style2" align="center"><strong>PIN Number</strong></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <asp:TextBox ID="txtBusinessID" runat="server"></asp:TextBox>
+            </td>
+            <td>
+                <asp:TextBox ID="txtBusinesPIN" runat="server"></asp:TextBox>
+            </td>
+            <td>
+                <asp:Button ID="Button1" runat="server" Text="Begin Registration" 
+                    onclick="Button1_Click" />
+            </td>
+            <td>
+                <asp:Label ID="lblError" runat="server" style="color: #FF0000; font-size: small" Text=""></asp:Label>
+            </td>
+        </tr>
+    </table>
+    
+    <p>
+        &nbsp;</p>
+    <p>
+        If you did not receive a licensing letter or if you would like to make your 
+        application in person or via an alternate method, please contact the City at:<br />
+    </p>
+    <p>
+        <br />
+        Department of Community and Economic Development<br />
+        8930 N. Government Way<br />
+        Hayden, Idaho&nbsp;
+        83835<br />
+        (208) 209-2022<br />
+        <span class="style3"></span><a 
+            href="mailto:businesslicensing@cityofhaydenid.us">businesslicensing@cityofhaydenid.us</a><br />
+    </p>
+    
+<p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+
+</asp:Content>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        txtBusinessID.Focus();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SearchForApplication();
+    }
+
+    private void SearchForApplication()
+    {
+        lblError.Text = "";
+
+        SqlConnection conn = new SqlConnection();
+
+        conn.ConnectionString = ConfigurationManager.ConnectionStrings["BusinessRegistrationDataReader"].ConnectionString;
+
+        SqlDataAdapter da;
+        SqlParameter workParam;
+
+        da = new SqlDataAdapter("sproc_GetApplicationDataByTempKeyAndPIN", conn);
+        da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+        //add the @TempKey input parameter 
+        workParam = new SqlParameter("@TempKey", System.Data.SqlDbType.Int);
+        workParam.Direction = ParameterDirection.Input;
+        try
+        {
+            workParam.Value = Convert.ToInt32(txtBusinessID.Text);
+        }
+        catch (Exception e)
+        {
+            string strErr1 = e.Message;
+            lblError.Text = "The data supplied does not match a record in the database. ";
+            workParam.Value = DBNull.Value;
+        }
+        da.SelectCommand.Parameters.Add(workParam);
+
+        //add the @ApplicationPIN input parameter 
+        workParam = new SqlParameter("@ApplicationPIN", System.Data.SqlDbType.NVarChar);
+        workParam.Direction = ParameterDirection.Input;
+        workParam.Value = txtBusinesPIN.Text;
+        da.SelectCommand.Parameters.Add(workParam);
+
+        //execute the stored procedure
+        DataSet ds = new DataSet();
+        try
+        {
+            da.Fill(ds);
+        }
+        catch (Exception e)
+        {
+            string strErr = e.Message;
+            lblError.Text += " There was a problem connecting to the data source.";
+        }
+
+        DataTable dt = new DataTable();
+
+        //create and populate the table that will be used to store the results
+        da.Fill(dt);
+
+        if (dt.Rows.Count > 0)
+        {
+            if (lblError.Text.Length < 1)
+            {
+                Response.Redirect("Exemptions.aspx?TempKey=" + dt.Rows[0]["TempID"].ToString() + "&PIN=" + dt.Rows[0]["ApplicationPIN"].ToString());
+                //Response.Redirect("ApplicationStep1.aspx?TempKey=" + dt.Rows[0]["TempID"].ToString() + "&PIN=" + dt.Rows[0]["ApplicationPIN"].ToString());
+            }
+        }
+        else
+        {
+            lblError.Text = "The data supplied does not match a record in the database. ";
+        }
+
+    }
+
+
+}
